2c0a72a5718f43c326fc603ff5e4d41185b4dc59
Daniel Andujar <daniel.ap252@gmail.com>
2020-05-07 11:00:44 -0400

    Updates

---
+++ precache-manifest.dc5950d93df7b205d6d5eba754194a82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5437e3583912d1764d4acc984aa02eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba3db772c930c5a3ab04",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "61deec61c6ae5cfd9adf",
+    "url": "/static/js/2.cc363e42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc363e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3db772c930c5a3ab04",
+    "url": "/static/js/main.c124a5bc.chunk.js"
+  },
+  {
+    "revision": "63ab7123d07ec1ae36ef",
+    "url": "/static/js/runtime-main.5e2f8f09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
